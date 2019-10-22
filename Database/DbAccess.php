ba66976bd1b6da4bfe121b27ed8a3d9a5ce16644
Rosario B <rossa_4057@hotmail.com>
2019-10-22 18:14:37 +0200

    Update DbAccess.php

--- Database/DbAccess.php
+++ Database/DbAccess.php
@@ -11,17 +11,23 @@ class DbAccess
 
     public static function GetPDO()
     {
-        $config = simplexml_load_file('Config/Hive.config.xml');
+        try {
+            $dsn = getenv('DATABASE_URL');
+            return new PDO($dsn);
 
-        $host = $config->Database->Host;
-        $port = $config->Database->Port;
-        $db = $config->Database->Name;
-        $db_user = $config->Database->UserName;
-        $db_pw = $config->Database->Password;
+        } catch (\Exception $ex) {
+            $config = simplexml_load_file('Config/Hive.config.xml');
 
-        $dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$db_user;password=$db_pw";
+            $host = $config->Database->Host;
+            $port = $config->Database->Port;
+            $db = $config->Database->Name;
+            $db_user = $config->Database->UserName;
+            $db_pw = $config->Database->Password;
 
-        return new PDO($dsn);
+            $dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$db_user;password=$db_pw";
+
+            return new PDO($dsn);
+        }
     }
 
 }

bf801ba04aa2fb1e95483eb83fbb01e2bc20bfa9
Leeeeroy <leeroy.koller@students.fhnw.ch>
2020-01-07 23:52:50 +0100

    change header and footer before login
    loginView updated

--- View/Layout/Header.php
+++ View/Layout/Header.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html>
 
+<?php
+if(isset($_SESSION['agentLogin'])) {
+
+?>
+
+
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -131,3 +138,30 @@
                     </ul>
                 </div>
             </nav>
+
+<?php
+
+} else {
+
+?>
+
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+                <title>Login - Brand</title>
+                <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+                <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+                <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+                <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+                <link rel="stylesheet" href="assets/css/Bootstrap-Tags-Input.css">
+                <link rel="stylesheet" href="assets/css/Drag--Drop-Upload-Form-1.css">
+                <link rel="stylesheet" href="assets/css/Drag--Drop-Upload-Form.css">
+                <link rel="stylesheet" href="assets/css/untitled.css">
+            </head>
+
+<?php
+
+}
+
+?>

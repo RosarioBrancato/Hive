f07b7416c2fbf50dea8767dbb9a635979cfa77c1
Rosario B <rossa_4057@hotmail.com>
2020-01-08 15:11:17 +0100

    Bugfix: Invalid HTML before Login - Footer

--- View/Layout/Footer.php
+++ View/Layout/Footer.php
@@ -1,46 +1,24 @@
-
 <?php
-if(isset($_SESSION['agentLogin'])) {
 
-    ?>
+use Service\AuthServiceImpl;
+
+?>
 
 
 </div>
-<footer class="bg-white sticky-footer">
-    <div class="container my-auto">
-        <div class="text-center my-auto copyright"><span>Copyright © Hive 2020</span></div>
-    </div>
-</footer>
+<?php if (AuthServiceImpl::getInstance()->getCurrentAgentId() > 0) { ?>
+    <footer class="bg-white sticky-footer">
+        <div class="container my-auto">
+            <div class="text-center my-auto copyright"><span>Copyright © Hive 2020</span></div>
+        </div>
+    </footer>
+<?php } ?>
+
 </div>
+</div>
+
 <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
 <script src="assets/js/Bootstrap-Tags-Input.js"></script>
 <script src="assets/js/theme.js"></script>
 <script src="assets/js/script.js"></script>
 </body>
-
-
-<?php
-
-} else {
-
-?>
-
-
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/js/chart.min.js"></script>
-<script src="assets/js/bs-charts.js"></script>
-<script src="assets/js/Bootstrap-Tags-Input-1.js"></script>
-<script src="assets/js/Bootstrap-Tags-Input.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-<script src="assets/js/theme.js"></script>
-</body>
-
-
-</html>
-
-<?php
-
-}
-
-?>
